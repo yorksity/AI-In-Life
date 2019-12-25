c6107f6344b72358777fdf5f3c21bbe27be3f92c
yorksiy <yorksity@outlook.com>
2019-12-26 00:17:57 +0800

    first commit

---
+++ src/main/java/com/yorksity/AiLife/AiLifeApplication.java
@@ -0,0 +1,13 @@
+package com.yorksity.AiLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yorksity/AiLife/AiLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yorksity.AiLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
